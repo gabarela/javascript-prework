5285a772b0386b7ab0dc0de31c95c51e441627c2
Gabriela Cichowicz <gabriela.cichowicz@gmail.com>
2020-02-28 10:36:31 +0100

    Add js for comp & player moves

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2'){
+  computerMove = 'papier';
+} else if (randumNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+  playerMove = 'papier';
+}else if (playerInput == '3'){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch: ' + playerMove);
